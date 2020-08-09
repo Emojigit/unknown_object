fff4b3c4e0b6d258bd946e7854d616ea25c575a0
Yiu Man Ho <yiufamily.hh@gmail.com>
2020-08-09 17:55:57 +0800

    Make the object lose its collisionbox (You can still point it

--- init.lua
+++ init.lua
@@ -2,15 +2,13 @@ minetest.register_entity("unknown_object:unknown_object", {
     hp_max = 1,
     physical = true,
     weight = 5,
-    collisionbox = {-0.5,-0.5,-0.5, 0.5,0.5,0.5},
+    -- collisionbox = {-0.5,-0.5,-0.5, 0.5,0.5,0.5},
     visual = "sprite",
     visual_size = {x=0.8, y=0.8},
     textures = {"unknown_object.png"}, -- number of required textures depends on visual
     colors = {}, -- number of required colors depends on visual
-    spritediv = {x=1, y=1},
     initial_sprite_basepos = {x=0, y=0},
     is_visible = true,
-    makes_footstep_sound = false,
 })
 
 minetest.register_craftitem("unknown_object:unknown_object_spawner", {
